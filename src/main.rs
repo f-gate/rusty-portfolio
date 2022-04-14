bf0fa84d124ed32fa82c90c70e865da7ab47b1cf
felix gate <felixgate13@gmail.com>
2022-04-14 12:36:03 +0100

    handlebars, index html, template

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,46 @@
 #[macro_use] extern crate rocket;
+use rocket::Request;
+use rocket::http::Status;
+use rocket_contrib::templates::Template;
 
 #[get("/")]
-fn index() -> &'static str {
+fn index() -> Template {
     "Hello, world!"
 }
 
-#[post("/algorithms")]
-fn algorithms() -> &'static str {
+#[get("/algorithms")]
+fn algorithms() -> Template {
     "Hello, world!"
+}
+
+#[get("/cryptography")]
+fn algorithms() -> Template {
+    "Hello, world!"
+}
+
+#[get("/data-structures")]
+fn algorithms() -> Template {
+    "Hello, world!"
+}
+
+#[catch(404)]
+fn not_found(req: &Request) {
+
+ }
+
+ #[catch(default)]
+fn default_catcher(status: Status, request: &Request) { 
+
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index])
                     .mount("/simple", routes![algorithms])
+                    .register("/", catchers![default_catcher])
+                    .register("/", catchers![not_found])
+                    .attach(Template::fairing())
 
 }
+
+
